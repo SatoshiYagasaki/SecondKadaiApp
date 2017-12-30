d9aedf51f1b5e79d8af7ba4596a4955b7a770c6f
矢ヶ崎哲史 <syaga2002@yahoo.co.jp>
2017-12-30 14:22:17 +0900

    とりあえず完成。

--- SecondKadaiApp/ResultViewController.swift
+++ SecondKadaiApp/ResultViewController.swift
@@ -20,7 +20,7 @@ class ResultViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     
-        label.text = inputName
+        label.text = "こんにちは、\(inputName!)さん。"
     
     }
 
